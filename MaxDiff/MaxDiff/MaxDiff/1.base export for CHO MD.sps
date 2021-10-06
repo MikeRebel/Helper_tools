3d1cebfc083fb85d298c2fbe40c98e88bc1193bb
Mike <56391191+MikeRebel@users.noreply.github.com>
2021-10-06 17:46:03 +0300

    start commit

---
+++ MaxDiff/MaxDiff/MaxDiff/1.base export for CHO MD.sps
@@ -0,0 +1,59 @@
+*¬ начале одна переменна€ - ключ дл€ слити€, потом все положительные выборы, потом все отрицательные выборы, потом дизайн.
+*¬ выборах номера концепций из дизайна.
+*¬ конце сохранить базу как Tab Del.
+SAVE OUTFILE='C:\Users\v.shchurov\Documents\work\2017\118-003-17\base_md.sav'
+  /keep=InterviewID
+InterviewID
+MD_1_1_Code
+MD_1_2_Code
+MD_2_1_Code
+MD_2_2_Code
+MD_3_1_Code
+MD_3_2_Code
+MD_4_1_Code
+MD_4_2_Code
+MD_5_1_Code
+MD_5_2_Code
+MD_6_1_Code
+MD_6_2_Code
+MD_7_1_Code
+MD_7_2_Code
+MD_8_1_Code
+MD_8_2_Code
+scr1_item1
+scr1_item2
+scr1_item3
+scr1_item4
+scr2_item1
+scr2_item2
+scr2_item3
+scr2_item4
+scr3_item1
+scr3_item2
+scr3_item3
+scr3_item4
+scr4_item1
+scr4_item2
+scr4_item3
+scr4_item4
+scr5_item1
+scr5_item2
+scr5_item3
+scr5_item4
+scr6_item1
+scr6_item2
+scr6_item3
+scr6_item4
+scr7_item1
+scr7_item2
+scr7_item3
+scr7_item4
+scr8_item1
+scr8_item2
+scr8_item3
+scr8_item4
+/COMPRESSED.
+
+
+
+
---
+++ export/launch_extract.bat
@@ -0,0 +1,48 @@
+extract.exe 1.lot
+ping 127.0.0.1 -n 5
+extract.exe 2.lot
+ping 127.0.0.1 -n 5
+extract.exe 3.lot
+ping 127.0.0.1 -n 5
+extract.exe 4.lot
+ping 127.0.0.1 -n 5
+extract.exe 5.lot
+ping 127.0.0.1 -n 5
+extract.exe 6.lot
+ping 127.0.0.1 -n 5
+extract.exe 7.lot
+ping 127.0.0.1 -n 5
+extract.exe 8.lot
+ping 127.0.0.1 -n 5
+extract.exe 9.lot
+ping 127.0.0.1 -n 5
+extract.exe 10.lot
+ping 127.0.0.1 -n 5
+extract.exe 11.lot
+ping 127.0.0.1 -n 5
+extract.exe 12.lot
+ping 127.0.0.1 -n 5
+extract.exe 13.lot
+ping 127.0.0.1 -n 5
+extract.exe 14.lot
+ping 127.0.0.1 -n 5
+extract.exe 15.lot
+ping 127.0.0.1 -n 5
+extract.exe 16.lot
+ping 127.0.0.1 -n 5
+extract.exe 17.lot
+ping 127.0.0.1 -n 5
+extract.exe 18.lot
+ping 127.0.0.1 -n 5
+extract.exe 19.lot
+ping 127.0.0.1 -n 5
+extract.exe 20.lot
+ping 127.0.0.1 -n 5
+extract.exe 21.lot
+ping 127.0.0.1 -n 5
+extract.exe 22.lot
+ping 127.0.0.1 -n 5
+extract.exe 23.lot
+ping 127.0.0.1 -n 5
+extract.exe 24.lot
+ping 127.0.0.1 -n 5
